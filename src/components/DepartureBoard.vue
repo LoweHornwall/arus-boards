f185b38a8b93c2bf018cbffacc1d1fb92a6f07bb
Lowe Hornwall <lowe.hornwall@gmail.com>
2025-04-19 14:09:07 +0200

    Fix scaling

--- src/components/DepartureBoard.vue
+++ src/components/DepartureBoard.vue
@@ -4,25 +4,29 @@
     <v-card-text class="text-center">
       <v-container class="pa-0 ma-0" fluid>
         <v-row no-gutters>
-          <v-col cols="5" align-self="center">
-            <div class="text-center">
-              <v-icon icon="mdi-arrow-left" />
-            </div>
-            <div style="height: 11rem" class="overflow-auto">
-              <departure-board-timetable
-                :departures="departureBoard.departuresDir1"
-              />
+          <v-col cols="6" align-self="center">
+            <div class="me-5">
+              <div class="text-center">
+                <v-icon icon="mdi-arrow-left" />
+              </div>
+              <div style="height: 11rem" class="overflow-auto">
+                <departure-board-timetable
+                  :departures="departureBoard.departuresDir1"
+                />
+              </div>
             </div>
           </v-col>
           <v-spacer />
-          <v-col cols="5">
-            <div class="text-center" align-self="center">
-              <v-icon icon="mdi-arrow-right" />
-            </div>
-            <div style="height: 11rem" class="overflow-auto">
-              <departure-board-timetable
-                :departures="departureBoard.departuresDir2"
-              />
+          <v-col cols="6">
+            <div class="ms-5">
+              <div class="text-center" align-self="center">
+                <v-icon icon="mdi-arrow-right" />
+              </div>
+              <div style="height: 11rem" class="overflow-auto">
+                <departure-board-timetable
+                  :departures="departureBoard.departuresDir2"
+                />
+              </div>
             </div>
           </v-col>
         </v-row>
--- src/components/DepartureBoardTimetableDeparture.vue
+++ src/components/DepartureBoardTimetableDeparture.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="d-flex align-center">
-    <div class="d-flex me-auto align-center">
-      <div
-        class="bg-red-darken-3 text-white py-1 px-1 rounded me-2"
-        style="width: 2.5rem"
-      >
-        {{ departure.line }}
-      </div>
-      <div>
-        {{ departure.direction }}
-      </div>
-    </div>
-    <div class="me-4" style="width: 6rem">{{ departure.time }}</div>
-    <v-icon icon="mdi-bus-clock" />
-    <div class="me-4" style="width: 2.5rem">{{ departure.timeRemaining }}</div>
-    <v-icon icon="mdi-walk" />
-    <div class="me-4" style="width: 2.5rem">
-      {{ departure.timeRemainingWalk }}
-    </div>
-  </div>
+  <v-container class="pa-0 ma-0" fluid>
+    <v-row no-gutters>
+      <v-col cols="6" align-self="center">
+        <div class="d-flex me-auto align-center">
+          <div
+            class="bg-red-darken-3 text-white py-1 px-1 rounded me-2"
+            style="width: 2.5rem"
+          >
+            {{ departure.line }}
+          </div>
+          <div>
+            {{ departure.direction }}
+          </div>
+        </div>
+      </v-col>
+      <v-spacer />
+      <v-col cols="2" align-self="center">
+        <div class="d-flex align-center">
+          <div style="min-width: 3rem">
+            {{ departure.time }}
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="2" align-self="center">
+        <div class="d-flex align-center">
+          <v-icon icon="mdi-bus-clock" />
+          <div style="min-width: 3rem">
+            {{ departure.timeRemaining }}
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="2" align-self="center">
+        <div class="d-flex align-center">
+          <v-icon icon="mdi-walk" />
+          <div style="min-width: 3rem">
+            {{ departure.timeRemainingWalk }}
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script setup lang="ts">
 import type { Departure } from "@/config/types";
