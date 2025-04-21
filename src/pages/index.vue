026e50102191309d794cb5309a2834af01e7e5eb
Lowe Hornwall <lowe.hornwall@gmail.com>
2025-04-21 02:10:07 +0200

    Move clock and alarm to v-app-bar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,17 @@
 <template>
+  <v-app-bar>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="3">
+          <digital-clock />
+        </v-col>
+        <v-col cols="6">
+          <departure-alarm />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app-bar>
   <v-container fluid>
-    <v-row>
-      <v-col cols="3">
-        <digital-clock />
-      </v-col>
-      <v-col cols="6">
-        <departure-alarm />
-      </v-col>
-    </v-row>
     <v-row>
       <v-col>
         <departure-boards />
